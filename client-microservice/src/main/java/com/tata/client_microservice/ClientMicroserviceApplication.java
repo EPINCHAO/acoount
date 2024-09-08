c412a183eca4ee3bbe58b05679e6a86e5de014d6
Edwin Pinchao <epinchao@krugerschool.edu.ec>
2024-09-08 17:42:08 -0500

    "Discover service config and Eureka Client on client-micreservice"

--- client-microservice/src/main/java/com/tata/client_microservice/ClientMicroserviceApplication.java
+++ client-microservice/src/main/java/com/tata/client_microservice/ClientMicroserviceApplication.java
@@ -1,5 +1,6 @@
 package com.tata.client_microservice;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
